7a7092ad28de4251d268c68ae1a0689827983c90
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2023-11-25 22:38:19 -0800

    small changes

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -8,7 +8,7 @@ public class Board {
     static Piece[][] board = new Piece[8][8];
     static int currentPlayer = 0; // 0 for white, 1 for black
 
-    public void changePlayer(){
+    public static void changePlayer(){
         currentPlayer = currentPlayer==0 ? 1: 0;
     }
 
@@ -68,16 +68,21 @@ public class Board {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Player "+currentPlayer+1+"'s turn. \n Please enter your move(EX: a2 a3)");
         String[] move = scanner.nextLine().split(" ");
-        int fromRow = ((int) move[0].charAt(1))-1;
-        int fromCol = move[0].charAt(0);
-        int toRow = move[1].charAt(1);
-        int toCol = move[1].charAt(0);
 
-
+        int fromRow = move[0].charAt(1)-'0'-1;
+        int fromCol = move[0].charAt(0)-'a';
+        int toRow = move[1].charAt(1)-'0'-1;
+        int toCol = move[1].charAt(0)-'a';
+        if(!isValidInput(fromRow, fromCol, toRow, toCol)) return;
         moveFigure(fromRow, fromCol, toRow, toCol);
+        changePlayer();
     }
 
     public static boolean isValidInput(int fromRow, int fromCol, int toRow, int toCol){
+        if(fromRow<0 || fromRow>7 || toRow<0 || toRow > 7 || fromCol<0 || fromCol>7 || toCol<0 || toCol > 7 ){
+            System.out.println("This is invalid input, please try again");
+            return false;
+        }
         return true;
     }
 
