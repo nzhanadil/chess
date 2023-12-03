beac27607c4126914bf7921997f6481ea7a35643
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2023-12-02 21:41:08 -0600

    fixing bug for moving the element, and upgrading moves

--- src/main/java/chess/figures/Knight.java
+++ src/main/java/chess/figures/Knight.java
@@ -17,28 +17,7 @@ public class Knight extends Piece implements Moves {
 
     @Override
     public void setAllAvailableMoves() {
-        //allAvailableMoves = shortMoves(directions, getRow(), getCol());
-        allAvailableMoves.clear();
-        allBackedUpPieces.clear();
-
-        for (int[] dir : directions) {
-            int dRow = dir[0];
-            int dCol = dir[1];
-            int x = getRow() + dRow;
-            int y = getCol() + dCol;
-
-            if (x >= 0 && x < 8 && y >= 0 && y < 8) {
-                if (board[x][y] == null) {
-                    allAvailableMoves.add(new int[]{x, y});
-                } else {
-                    if (!board[x][y].getColor().equals(getColor())) {
-                        allAvailableMoves.add(new int[]{x, y});
-                    } else {
-                        allBackedUpPieces.add(board[x][y]);
-                    }
-                }
-            }
-        }
+        setAllAvailableMovesForShortMovingPieces(directions, getRow(), getCol());
     }
 
 }
