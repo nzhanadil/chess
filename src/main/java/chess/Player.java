6feed90110c947b3df60ef5bc02c6e71c38983bf
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2023-12-03 20:41:46 -0600

    created player object, cleaned up board class and moved to main

---
+++ src/main/java/chess/Player.java
@@ -0,0 +1,28 @@
+package chess;
+
+public class Player {
+
+    String color;
+    String name;
+    boolean isUnderCheck;
+    boolean isMate;
+
+    public Player(String color, String name){
+        this.color = color;
+        this.name = name;
+        this.isUnderCheck = false;
+        this.isMate = false;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isUnderCheck() {
+        return isUnderCheck;
+    }
+}
