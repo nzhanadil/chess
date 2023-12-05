eada5bec5b8eb5ae434ba2693e8ff8b5350a3e8f
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2023-12-05 15:55:53 -0600

    created Player abstract class, Human and AI player objects

---
+++ src/main/java/chess/players/HumanPlayer.java
@@ -0,0 +1,13 @@
+package chess.players;
+
+public class HumanPlayer extends Player {
+
+    public HumanPlayer(String color, String name, String type) {
+        super(color, name, type);
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
